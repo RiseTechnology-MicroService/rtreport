dd550c59564b3d8aee1922db0130e9f64e26f115
ihsan Ç <a.ihsan.celik96@gmail.com>
2023-06-09 19:11:26 +0300

    Created Generic Response

---
+++ Responses/GenericResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace rtreport.Responses
+{
+    public class GenericResponse
+    {
+        public bool Success => ErrorMessages.Count == 0;
+        public List<string> ErrorMessages { get; set; }
+
+        public GenericResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+
+        public void SetFailure(List<string> errorMessages) => ErrorMessages = errorMessages;
+
+    }
+    public class GenericResponse<T> : GenericResponse
+    {
+        public T? Data { get; set; }
+        public void SetSuccess(T data) => Data = data;
+    }
+}

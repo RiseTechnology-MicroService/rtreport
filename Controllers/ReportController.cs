0cd6ae0c4edd283a54239feb3f500e733833c4fa
ihsan Ç <a.ihsan.celik96@gmail.com>
2023-06-09 19:16:42 +0300

    Create ReportController.cs

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rtreport.Models;
+using rtreport.Services;
+
+namespace rtreport.Controllers
+{
+    [ApiController, Route("api/report")]
+    public class ReportController : ControllerBase
+    {
+        private readonly ReportService reportService;
+        public ReportController(ReportService reportService)
+        {
+            this.reportService = reportService;
+        }
+
+        [HttpGet, Route("list")]
+        public List<ReportSummaryModel> GetReportList()
+        {
+            return reportService.GetSummaryList();
+        }
+
+        [HttpGet, Route("detail/{requestId}")]
+        public ReportDetailModel GetReportDetail(string requestId)
+        {
+            return reportService.GetReportDetail(requestId);
+        }
+    }
+}
